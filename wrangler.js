cc03c82a9412de6bee440f6ccc1848bfeea2bb11
James Rutledge <jarutledge2@gmail.com>
2017-08-12 06:50:06 -0400

    it works but i have no idea why

--- wrangler.js
+++ wrangler.js
@@ -15,66 +15,96 @@ var mapPath, map, thisPath, stats, mdStats;
 
 // mother function
 function wranglerInit(map) {
+	let newMap = { rootDir: map.rootDir, data: null, meta: map.meta };
 
-	// console.log(map.rootDir)
-	// console.log(map.meta)
+	mainLoop(map.rootDir, map.data, (data) => { // console.log(data)
 
-	mainLoop(map.rootDir, map.data, (newMap) => {
+		newMap.data = data
+
 		mainUtil.writeFile([__dirname, 'data'], 'deep_map', 'json', newMap);
+
 	})
 
 }
 
 
 function mainLoop(thisDir, tree, callback) {
+	// If dirs prop, files prop both exist, and both are not null
+	if (tree.hasOwnProperty('dirs') && tree['dirs'] != null && tree.hasOwnProperty('files') && tree['files'] != null) {
+		// resolve multiple promises
+		Promise.all([dirLoop(thisDir, tree), fileLoop(thisDir, tree)]).then(() => { // console.log('ALL PROMISE SUCCESS!!!')
 
-	// console.log({thisDir})
+			callback(tree)
 
-	if (tree.hasOwnProperty('dirs') && tree['dirs'] != null) {
-		dirLoop(thisDir, tree, (newTree) => { tree = newTree })
+		})
+	// If 
+	} else if (tree.hasOwnProperty('dirs') && tree['dirs'] != null && (!tree.hasOwnProperty('files') || tree['files'] == null)) {
+
+		callback(dirLoop(thisDir, tree))
+
+	} else if (tree.hasOwnProperty('files') && tree['files'] != null && (!tree.hasOwnProperty('dirs') || tree['dirs'] == null)) {
+
+		callback(fileLoop(thisDir, tree))
+
+	} else {
+
+		console.log(`ERROR: UNMAPPED DIR`)
+
 	}
 
-	if (tree.hasOwnProperty('files') && tree['files'] != null) {
-		fileLoop(thisDir, tree, (newTree) => { tree = newTree })
-	}
-
-
-	callback(tree)
 }
 
 function dirLoop(parentDir, dirObj, callback) { // console.log(`this dir, ${parentDir}, has dirs.`)
-
+	// create a dir arr for new dir formations
+	let dirArr = [];
+	// loop over the dirs
 	for (dir in dirObj.dirs) {
-		mainLoop(path.join(parentDir, dir), dirObj.dirs[dir], (tree) => { dirObj.dirs[dir] = tree })
+		// call mainLoop to check dirs for files & sub dirs
+		mainLoop(path.join(parentDir, dir), dirObj.dirs[dir], (newDir) => {
+			// push to dirArr within callback
+			dirArr.push(newDir)
+
+		})
 
 	}
-
-	callback(dirObj)
+	// return dirArr
+	return dirArr
 }
 
-function fileLoop(parentDir, dirObj, callback) { // console.log(`this dir, ${parentDir}, has files.`)
-	dirObj['fileStats'] = {};
+function fileLoop(parentDir, dirObj) { // console.log(`this dir, ${parentDir}, has files.`)
 
-	dirObj.files.forEach((file) => {
-		// combine the parent directory path and file name to get the relative path for the file
-		thisPath = path.join(parentDir, file)
-		// get the file system stats
-		stats = mapUtil.fsStats(thisPath)
-		// add these to the new fileStats dict
-		dirObj.fileStats[file] = { relPath: thisPath, stats }
+	return new Promise((success, err) => {
+		// create a filesStats obj in dir
+		dirObj['fileStats'] = {};
 
-		// now create a promise for the metadata
-		mdStats = new Promise((resolve, reject) => {
-			// spawn the metadata list command, and resolve promise within callback
-			mapUtil.spawnMDLS(thisPath, (metaData) => { resolve(metaData) })
+		for (var i = 0; i < dirObj.files.length; i++) {
+			// local vars
+			let fileName = dirObj.files[i];
+			// combine the parent directory path and file name to get the relative path for the file
+			thisPath = path.join(parentDir, fileName)
+			// get the file system stats
+			stats = mapUtil.fsStats(thisPath)
+			// add these to the new fileStats dict
+			dirObj.fileStats[fileName] = { relPath: thisPath, stats, meta: null }
 
-		})
-		mdStats.then((data) => {
-			// then add metadata to the new fileStats dict
-			dirObj.fileStats[file]['meta'] = data
-			// send it through the callback
-			callback(dirObj);
-		})
+			// now create a promise for the metadata
+			mdStats = new Promise((resolve, reject) => {
+				// spawn the metadata list command, and resolve promise within callback
+				mapUtil.spawnMDLS(thisPath, (metaData) => {
+					// then add metadata to the new fileStats dict
+					dirObj.fileStats[fileName].meta = metaData
+
+					resolve(dirObj)
+				})
+
+			})
+			mdStats.then((dirObj) => {
+				// If it's the last file, resolve outer promise
+				if (i == dirObj.files.length) { success(dirObj) }
+
+			})
+
+		}
 
 	})
 
